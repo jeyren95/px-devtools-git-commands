7302dae9f6cf603b96917afd30fd5cc11e0a0ae3
Jey-ren <jeyren95@hotmail.com>
2021-09-05 20:14:06 +0800

    test: edit server.js message

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const requestListener = function (req, res) {
     res.writeHead(200);
     // OS assist to send this info from the node to browser
     // browser will then read this file and render it on the browser
-    res.end("My first server! Hello world!");
+    res.end("My first server! This is an edited message");
 };
 
 // shell will find a program name that matches "node"
@@ -17,7 +17,7 @@ const requestListener = function (req, res) {
         // by default, they cannot communicate w each other
         // OS will assist the browser, send a message to the node to send back to the browser
 
-// the node server is listening on a port/socket
+// the node server is listening on a port
     // every unique process is listening on a different port
 // browser makes a http request on port 8000, and sends the request to the browser
 // the browser then sends a message to the node server
